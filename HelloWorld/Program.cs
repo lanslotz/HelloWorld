5e8b8e961f70bdda4d2d40782a9a6f7341f535e6
zhenw <zhenw@DESKTOP-A960KRD>
2020-01-22 22:45:03 -0500

    Version 2 of HelloWorld

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Pause to display 'Hello World!'
-            Console.WriteLine("Press Any Key to Continue...");
+            Console.WriteLine("Press Any Key to Continue....");
             Console.WriteLine("Hello World!");
             Console.ReadKey(true);
         }
