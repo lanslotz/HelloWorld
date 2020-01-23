c1a10fab31c32c9aef566ea603716685795e2c63
zhenw <zhenw@DESKTOP-A960KRD>
2020-01-22 22:41:04 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display 'Hello World!'
+            Console.WriteLine("Press Any Key to Continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
